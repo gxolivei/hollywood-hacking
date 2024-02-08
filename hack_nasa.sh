6b8fb4888e5f0fe9cc4ab7c1684a0db362481ef1
Gabriel Oliveira <gxolivei@gmail.com>
2024-02-08 18:27:07 -0300

    top secret commit

---
+++ hack_nasa.sh
@@ -0,0 +1,14 @@
+echo "HACKING NASA 0%"
+sleep 1
+
+clear
+
+for i in {5..100..5}
+do
+    echo -ne "HACKING NASA $i%\r"
+    sleep 0.1
+done
+
+clear
+
+echo "HACKING NASA COMPLETED - ACCESS GRANTED"
